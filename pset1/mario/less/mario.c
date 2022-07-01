22d1f239998e156156e0542e3ae2017d2a5ed501
Rafael Euzebio <rafaeleuzebiomendes@protonmail.com>
2022-07-01 03:49:21 -0300

    rejecting numbers lower than 1

--- pset1/mario/less/mario.c
+++ pset1/mario/less/mario.c
@@ -1,18 +1,31 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int get_height();
+
 int main(void) 
 {
-    int height = get_int("Height ");     
+    int height = get_height();
+    while(height > 8 || height < 1)
+    {
+        height = get_height();
+    }
 
     for(int i = 0; i < height; i++){
 
-        for(int s = height - i; s > 1; s--){
+        for(int spaces = height - i; spaces > 1; spaces--){
             printf(" ");
         }
+
         for(int k = 0; k <= i; k++){
             printf("#");     
         }
         printf("\n");
     }
+}
+
+int get_height()
+{
+    return get_int("Height ");     
+    
 }
